dfbaf886080fea85615a15adbbbae5b9054479ef
Gabriela dos Santos <gabisinhas@yahoo.com.br>
2025-02-19 16:48:08 -0300

    vuelidate

--- front-vue/src/components/create-employee/CreateEmployee.vue
+++ front-vue/src/components/create-employee/CreateEmployee.vue
@@ -5,13 +5,15 @@
         <h3>Add Employee</h3>
       </div>
       <div class="card-body">
-        <form v-on:submit.prevent="add">
+        <form v-on:submit.prevent="handleSubmitForm()">
           <div class="form-group">
             <label class="font-weight-bold">Employee Name</label>
             <input
               type="text"
               class="form-control"
               placeholder="Type your Name"
+              v-model="employee.name"
+              require
             />
           </div>
           <div class="form-group">
@@ -20,6 +22,8 @@
               type="text"
               class="form-control"
               placeholder="Type your Job Role"
+              v-model="employee.job_role"
+              required
             />
           </div>
           <div class="form-group">
@@ -28,11 +32,19 @@
               type="number"
               class="form-control"
               placeholder="Type your Salary"
+              v-model="employee.salary"
+              required
             />
           </div>
           <div class="form-group">
             <label class="font-weight-bold">Birth</label>
-            <input type="date" class="form-control" placeholder="YYYY/MM/DD" />
+            <input
+              type="date"
+              class="form-control"
+              placeholder="YYYY/MM/DD"
+              v-model="employee.birth"
+              required
+            />
           </div>
           <div class="form-group">
             <label class="font-weight-bold">Employee Registration</label>
@@ -40,10 +52,14 @@
               type="text"
               class="form-control"
               placeholder="Employee Registration"
+              v-model="employee.employee_registration"
+              required
             />
           </div>
           <div class="form-group">
-            <button class="btn btn-primary" type="submit">Add Employee</button>
+            <button class="btn btn-primary" type="submit">
+              + Add Employee
+            </button>
           </div>
         </form>
       </div>
@@ -55,7 +71,24 @@ export default {
   component: {
     name: "CreateEmployee",
   },
-  setup() {},
+  setup() {
+    const employee = {
+      name: "",
+      job_role: "",
+      salary: "",
+      birth: "",
+      employee_registration: "",
+    };
+
+    function handleSubmitForm() {
+      console.log("Save");
+    }
+
+    return {
+      employee,
+      handleSubmitForm,
+    };
+  },
 };
 </script>
 <style scoped>
--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-// import VueSweetalert2 from "vue-sweetalert2";
+import Vuelidate from "vuelidate";
+import VueSweetalert2 from "vue-sweetalert2";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/nprogress/nprogress.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router, Vuelidate, VueSweetalert2).mount("#app");
